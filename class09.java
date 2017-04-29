35fff866697f7503e500f4186c216718918e8cea
FrRushu <frushu@gmail.com>
2017-04-29 22:39:49 +0600

    While loop

--- class09.java
+++ class09.java
@@ -3,7 +3,7 @@
  */
 public class class09 {
     public static void main(String args[]){
-        //While loop
+        //While loop demo
         int counter=0;
         while (counter<5){
             System.out.println("lol "+counter);
