8c3b774329ba8e221bfe8995a68765322a7e1897
FrRushu <frushu@gmail.com>
2017-04-29 23:29:16 +0600

    Do While loop

---
+++ class10.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Rushu on 4/29/2015.
+ */
+public class class10 {
+    public static void main(String args[]){
+        //in the do while loop, even before checking the condition the program will execute the do part
+        int counter=100;
+        do{
+            System.out.println(counter);
+            counter++;
+
+        }while (counter<10);
+    }
+}
