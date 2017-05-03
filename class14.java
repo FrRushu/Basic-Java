4530ab68e37084920aa6a0a5d4d403896cd02767
FrRushu <frushu@gmail.com>
2017-05-03 21:55:12 +0600

    Math Class Mehtod

---
+++ class14.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Rushu on 5/3/2015.
+ * math class methods
+ */
+public class class14 {
+
+    public static void main(String args[]){
+        //Math is an builtin class in java , which has all the math operations
+        //absolute value
+        System.out.println(Math.abs(-24.8));
+        //rounding number in ceiling
+        System.out.println(Math.ceil(14.8));
+        //rounding number floor
+        System.out.println(Math.floor(14.8));
+        //finding max
+        System.out.println(Math.max(20,30));
+        //finding min
+        System.out.println(Math.min(20,30));
+        //power method 1st paremeter is the value, 2nd paremeter is the power (2^3) --->  (2,3) = 8
+        System.out.println(Math.pow(2,3));
+        //square root method
+        System.out.println(Math.sqrt(16));
+
+    }
+}
