a87b0ac3bd1473913ccce1760147bf0debaba139
FrRushu <frushu@gmail.com>
2017-05-03 22:35:25 +0600

    Another procedure of declaring an array

---
+++ class18.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Rushu on 5/3/2015.
+ */
+public class class18 {
+    public static void main(String args[]){
+        //this is another procedure of declaring an array followed by "class 16"
+        int arrayName [] = {1,2,3,4,5};
+        //output will be 4 because array index start from zero ;)
+        System.out.println(arrayName[3]);
+    }
+}
