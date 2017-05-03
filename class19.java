2f84079ad022343482ade5b37e40ef10cd93d021
FrRushu <frushu@gmail.com>
2017-05-03 22:36:40 +0600

    Array Table

---
+++ class19.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Rushu on 5/3/2015.
+ */
+public class class19 {
+    public static void main(String args[]){
+
+        int arrayName [] = {1,2,3,4,5};
+        System.out.println("Index" +"\t"+ "Value");
+        for (int counter = 0; counter < arrayName.length; counter++ ){
+            // \t is indecating tab
+            System.out.println(counter +"\t\t"+ arrayName[counter]);
+        }
+
+    }
+}
