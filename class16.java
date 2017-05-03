4ff74b2df76b44c81e675b46d672a8ef00de50c8
FrRushu <frushu@gmail.com>
2017-05-03 22:12:51 +0600

    Introduction to  Arrays

---
+++ class16.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Rushu on 5/3/2015.
+ * introduction to arrays
+ */
+public class class16 {
+    public static void main(String args []){
+        int arrayName[] = new int[10];
+        arrayName[2]=23;
+        //printing a specific index of an array 
+        System.out.println(arrayName[2]);
+    }
+}
