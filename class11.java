85f24ef56ee5a1e0c12fa96db1e6f680e82756b2
FrRushu <frushu@gmail.com>
2017-05-03 20:55:41 +0600

    Conditional Operators

---
+++ class11.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Rushu on 5/3/2015.
+ */
+public class class11 {
+    public static void main(String args []){
+        int age =50;
+        //                 condition      true              false
+        System.out.println(age > 60 ? "You are old" : "You are young");
+    }
+}
