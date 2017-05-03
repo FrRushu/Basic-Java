b9b61ef65a5c25b98295430e0e4fc660744d7844
FrRushu <frushu@gmail.com>
2017-05-03 21:14:38 +0600

    For Loop

---
+++ class12.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Rushu on 5/3/2015.
+ */
+public class class12 {
+    public static void  main(String args[]){
+        for(int counter=0; counter<5;counter++){
+            System.out.println("This is a for loop "+ counter);
+        }
+    }
+}
