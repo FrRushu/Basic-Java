c1df63d8f3d88ecc6322af8d50a260ce086bb2cd
FrRushu <frushu@gmail.com>
2017-05-04 10:56:45 +0600

    Polymorphism -Method Overloading

---
+++ class31.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Rushu on 5/4/2015.
+ */
+public class class31 {
+    public static void main(String args []){
+        class31_overloading demo=new class31_overloading();
+        demo.print();
+        demo.print("Rabbi");
+    }
+}
---
+++ class31_overloading.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Rushu on 5/4/2015.
+ */
+public class class31_overloading {
+    public void print(){
+        System.out.println("Hello Rushu");
+    }
+    public void print(String s){
+        System.out.println("Hi " + s);
+    }
+}
